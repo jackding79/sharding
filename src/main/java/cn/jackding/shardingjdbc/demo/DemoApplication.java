6a7af1ad36209b10b1f65bf61b305f86eb845034
jackding79 <407119315@qq.com>
2021-05-19 17:27:22 +0800

    ddddd

--- src/main/java/cn/jackding/shardingjdbc/demo/DemoApplication.java
+++ src/main/java/cn/jackding/shardingjdbc/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package cn.jackding.shardingjdbc.demo;
 
 
 import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.infra.hint.HintManager;
 import org.apache.shardingsphere.transaction.context.TransactionContexts;
 import org.apache.shardingsphere.transaction.core.TransactionType;
 import org.apache.shardingsphere.transaction.core.TransactionTypeHolder;
@@ -16,27 +17,17 @@ import java.time.LocalTime;
 
 public class DemoApplication {
 
-   // static String yamlFile ="E:\\idea\\code\\shardingjdbc\\src\\main\\resources\\application-sharding.yml";
-      static  String yamlFile ="E:\\code\\sharding-master\\src\\main\\resources\\application-sharding.yml";
+    static String yamlFile ="E:\\idea\\code\\shardingsphere\\sharding\\src\\main\\resources\\application-sharding.yml";
+    //  static  String yamlFile ="E:\\code\\sharding-master\\src\\main\\resources\\application-sharding.yml";
     public static void main(String[] args) throws Exception {
-       /* DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(new File(yamlFile));
-        String sql = "select * from t_order where user_id = ? and order_id = ?";
-        Connection connection = dataSource.getConnection();
-        PreparedStatement ps = connection.prepareStatement(sql);
-        ps.setInt(1,3);
-        ps.setInt(2,12);
-        ResultSet resultSet = ps.executeQuery();
-        while(resultSet.next()){
-            System.out.println(resultSet.getString("user_id"));
-        }*/
-       insert();
 
+        insertUser();
     }
 
-    public static void insert() throws Exception{
+    public static void insertOrder() throws Exception{
         //TransactionTypeHolder.set(TransactionType.XA);
         DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(new File(yamlFile));
-        String sql = "insert into t_user values(?,?,?,?,?,?)";
+        String sql = "insert into t_order values(?,?,?,?,?,?)";
         Connection connection =dataSource.getConnection();
         connection.setAutoCommit(false);
         PreparedStatement ps = connection.prepareStatement(sql);
@@ -52,4 +43,17 @@ public class DemoApplication {
         System.out.println(num);
     }
 
+    public static void insertUser() throws Exception{
+        HintManager manager =HintManager.getInstance();
+        manager.addDatabaseShardingValue("t_user",3);
+        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(new File(yamlFile));
+        String sql = "insert into t_user values(?,?,?)";
+        Connection connection =dataSource.getConnection();
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1,2);
+        ps.setString(2,"jack");
+        ps.setInt(3,9);
+        ps.executeUpdate();
+        connection.commit();
+    }
 }
